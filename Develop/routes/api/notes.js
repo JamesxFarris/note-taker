0b10802d69cc8ff983cc022aead9e73ab0d6cce8
James Farris <jafarris.exe@gmail.com>
2023-07-19 23:08:50 -0400

    added a try-catch to get request

--- Develop/routes/api/notes.js
+++ Develop/routes/api/notes.js
@@ -8,9 +8,18 @@ const router = express.Router();
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 router.get("/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
-    const notes = JSON.parse(data);
-    res.json(notes);
+    if (err) {
+      console.error("Cannot read file", err);
+      return res.status(500).json("Cannot read file");
+    }
+
+    try {
+      const notes = JSON.parse(data);
+      res.json(notes);
+    } catch (err) {
+      console.error("Error handling the JSON data", err);
+      res.status(500).json("Error handling the JSON data");
+    }
   });
 });
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file,
